cd9340fa297fc3ba569328a021e5093b8efa2b02
Brock Benson <brockjbenson@outlook.com>
2023-02-09 12:03:13 -0600

    server and route

--- server/routes/category.router.js
+++ server/routes/category.router.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
+require("dotenv").config();
+const axios = require("axios");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // return all categories
   const queryText = `SELECT * FROM category ORDER BY name ASC`;
   pool
@@ -14,6 +16,19 @@ router.get('/', (req, res) => {
     .catch((error) => {
       console.log(`Error on query ${error}`);
       res.sendStatus(500);
+    });
+});
+
+router.get("/test", (req, res) => {
+  let { qParam } = req.body;
+  axios
+    .get(
+      `https://api.giphy.com/v1/gifs/search?q=${qParam}&api_key=${process.env.API_KEY}`
+    )
+    .then((response) => res.send(response.data))
+    .catch((err) => {
+      res.sendStatus(500);
+      console.log(err);
     });
 });
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App/App';
+// import { Provider } from "react-redux";
+// import logger from "redux-logger";
+// import { takeEvery, put, take } from "redux-saga/effects";
+// import { createStore, combineReducers, applyMiddleware } from "redux";
+// // SAGA middlewhere 
+// import createSagaMiddleware from "redux-saga";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const sagaMiddleware= createSagaMiddleware();
+
+// function * rootSaga(){
+//     // takeEvery goes here
+// }
+
+
+
+
+
+// const storeInstance= createStore(
+//     ({
+//         // reducers go here
+
+//     })
+// )
+// //middleWare
+// applyMiddleware(logger,sagaMiddleware)
+
+
+// ReactDOM.render(
+//     <Provider store={storeInstance}>
+// <App /> </Provider>, document.getElementById('root'));
